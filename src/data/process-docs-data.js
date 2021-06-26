19b7a5850fd001849623fce290023e257e907675
David Armendariz <darmendariz1998@outlook.com>
2021-06-26 13:58:08 -0500

    Added process-docs-data

---
+++ src/data/process-docs-data.js
@@ -0,0 +1,26 @@
+export default class ProcessDocsData {
+  constructor(json) {
+    this.data = json.data?.docs || {};
+  }
+
+  getProcessedData(limit = 20, offset = 0) {
+    const dataToFetch = this.data.slice(offset, offset + limit);
+    return dataToFetch.reduce((processedData, row) => {
+      const processedRow = {};
+      processedRow.id = row.id;
+      processedRow.dealId = row.deal_id;
+      processedRow.documentName = row.doc_name;
+      processedRow.tag = null; // Not present
+      processedRow.posted = row.posted;
+      processedRow.lastAccessed = row.last_accessed;
+      processedRow.note = row.source_file?.note; // email note or source file note?
+      processedRow.file_path = this.getFilePath(row); // folder path + file name
+      processedData.push(processedRow);
+      return processedData;
+    }, []);
+  }
+
+  getFilePath(row) {
+    return `${row.folder_path}/${row.name}`;
+  }
+}
