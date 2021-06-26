f529dfc5fbc91bb719c7636b1023bae31d7facd0
David Armendariz <darmendariz1998@outlook.com>
2021-06-26 13:26:56 -0500

    Added data and ProcessDealsData class

---
+++ src/data/process-deals-data.js
@@ -0,0 +1,58 @@
+export default class ProcessDealsData {
+  constructor(json) {
+    this.data = json.data || {};
+    this.holdings = this.data.Holdings || [];
+    this.industries = this.buildHashMap('Industries', 'Id');
+    this.clientIssuers = this.buildHashMap('ClientIssuers', 'IssuerId');
+    this.agents = this.buildHashMap('Agents', 'Id');
+    this.sources = this.buildHashMap('Sources', 'Id');
+    this.dealTypes = this.buildHashMap('DealTypes', 'Id');
+    this.analysts = this.buildHashMap('Analysts', 'UserId');
+  }
+
+  buildHashMap(key, idKey) {
+    const attributeData = this.data[key] || [];
+    return attributeData.reduce((result, row) => {
+      const id = row[idKey];
+      result[id] = row;
+      return result;
+    }, {});
+  }
+
+  getProcessedData(limit = 20, offset = 0) {
+    const dataToFetch = this.holdings.slice(offset, offset + limit);
+    return dataToFetch.reduce((processedData, row) => {
+      const processedRow = {};
+      processedRow.id = row.Id;
+      processedRow.issuer = this.clientIssuers[row.IssuerId]?.IssuerName;
+      processedRow.dealName = row.DealName;
+      processedRow.bloombergId = null; // Not present
+      processedRow.isIn = null; // Not present
+      processedRow.customDealIdentifiers = row.CustomIdentifiers;
+      processedRow.customIssuerIdentifiers = row.CustomClientIssuersIdentifiers;
+      processedRow.industry = this.industries[row.IndustryId]?.IndustryName;
+      processedRow.agent = this.agents[row.AgentId]?.CompanyName;
+      processedRow.source = this.sources[row.SourceId]?.SourceName;
+      processedRow.status = null; // Not present
+      processedRow.total = row.Total;
+      processedRow.lastPosted = row.LastPosted;
+      processedRow.lastAccessed = row.LastAccessed;
+      processedRow.analysts = this.getAnalystsFromIds(row.AnalystIds);
+      processedRow.docCount = row.DocCount;
+      processedRow.customField = row.ClientCustomField;
+      // dealId to link it to a document
+      processedRow.dealId = row.DealId;
+      processedData.push(processedRow);
+      return processedData;
+    }, []);
+  }
+
+  getAnalystsFromIds(ids) {
+    return (ids || []).reduce((result, currentId) => {
+      if (this.analysts[currentId]?.FullName) {
+        result.push(this.analysts[currentId].FullName);
+      }
+      return result;
+    }, []);
+  }
+}
